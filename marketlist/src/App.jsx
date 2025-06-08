be39ccc18307e21d4d4082220f0096becb630310
durudaniel <danielchiemela16@gmail.com>
2025-06-08 06:39:37 +0100

    Rendering of components and applying css styles to them in my market list project

--- marketlist/src/App.jsx
+++ marketlist/src/App.jsx
@@ -1,5 +1,78 @@
 import "./App.css";
-
+import { useState } from "react";
+const intialListItem = [
+  { title: "i want to go to the market", id: 1234, checked: false },
+  { title: "i want to go to the church", id: 1235, checked: false },
+  { title: "attend to a prom date tonight", id: 1237, checked: false },
+  { title: "i want to go to the market", id: 1236, checked: false },
+];
 export default function () {
-  return <h1>Hello world</h1>;
+  const [list, setList] = useState(intialListItem);
+  return (
+    <div className="app">
+      <ListLogo>🏪 Market Carter 🛒</ListLogo>
+      <ListStats />
+      <ListForm />
+      <ListContainer list={list} />
+    </div>
+  );
+}
+
+function ListLogo({ children }) {
+  return <h1 className="logo">{children}</h1>;
+}
+
+function ListStats() {
+  return <h2 className="stats">Number Of Task (0/0)</h2>;
+}
+
+function ListForm() {
+  const [title, setTitle] = useState("");
+
+  const id = crypto.randomUUID();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const addNewList = {
+      title,
+      id,
+      checked: false,
+    };
+    setTitle("");
+
+    console.log(addNewList);
+  }
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="add task..."
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button className="btn">Submit</button>
+    </form>
+  );
+}
+
+function ListContainer({ list }) {
+  return (
+    <div className="container">
+      <ul>
+        {list.map((lists) => (
+          <Lists list={lists} key={lists.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Lists({ list }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <p>{list.title}</p>
+      <span>❌</span>
+    </li>
+  );
 }
